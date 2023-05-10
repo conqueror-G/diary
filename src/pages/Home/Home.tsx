bbde0b94e6a79a3adb8c68370e5e2989de8c7a4a
conquerorg <gitconquerorg@gmail.com>
2023-05-10 23:29:30 +0900

    study: default export와 named Export

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,8 +2,14 @@ import { useTranslation } from 'react-i18next'
 
 import { Box } from 'src/elements'
 
+import { 동물 as _별칭동물 } from './helpers'
+import 나비행기아닌데 from './helpers/defaultExport'
+
 export const Home = () => {
   const { t } = useTranslation()
+
+  const a = 나비행기아닌데
+  const b = _별칭동물
 
   return <Box>{t('Home')}</Box>
 }
---
+++ src/pages/Home/helpers/defaultExport.ts
@@ -0,0 +1,6 @@
+const 비행기 = {
+  name: '제트기',
+  cos: 11111,
+}
+
+export default 비행기
